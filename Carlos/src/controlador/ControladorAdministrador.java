a2cc963f9b1afa55d14df522d8ff4b0cb61d57e5
“Carpeka” <carlosperezc777@gmail.com>
2022-05-26 08:47:49 +0200

    Cambio1

--- Carlos/src/controlador/ControladorAdministrador.java
+++ Carlos/src/controlador/ControladorAdministrador.java
@@ -1,7 +1,24 @@
 package controlador;
 
+import ProyectoADM.vista.Administrador;
+import modelo.dao.usuario.Usuario;
+
 public class ControladorAdministrador {
-    public static void main(String[] args) {
-        System.out.println("David feo");
+    private Administrador vista;
+    private Usuario modelo;
+
+    public ControladorAdministrador(Administrador vista, Usuario modelo) {
+        this.vista = vista;
+        this.modelo = modelo;
     }
+
+    private void crearUsuario()
+    {
+
+    }
+
+
+
+
+
 }
