c2c0d93dab0238d8ef164e996fea138edf2f3f21
DavidMF99 <davidfernandez835@gmail.com>
2022-05-26 10:05:16 +0200

    actualizacion

--- David/src/controlador/Administrador/ControladorAdministrador.java
+++ David/src/controlador/Administrador/ControladorAdministrador.java
@@ -1,4 +1,31 @@
 package controlador.Administrador;
 
+import modelo.dao.usuario.UsuarioDAO;
+
+import javax.swing.*;
+
 public class ControladorAdministrador {
+    private UsuarioDAO modelo;
+    public ControladorAdministrador(UsuarioDAO modelo) {
+        this.modelo = modelo;
+        inicializarVista();
+    }
+
+    private void inicializarVista() {
+    }
+    public void inicializarControlador() {
+    }
+    private void salirApp() {
+        JButton botonSalir = new JButton("SALIR");
+        //usando una expresión labmda
+        botonSalir.addActionListener( actionEvent -> {
+            int opcion = JOptionPane.showInternalConfirmDialog(null,"¿Realmente quieres salir?",
+                    "CONFIRMACIÓN", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+            System.out.println(opcion);
+            if (opcion == 1)
+                return;
+            System.exit(0);
+            //(si/no)
+        });
+    }
 }
